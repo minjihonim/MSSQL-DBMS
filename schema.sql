6333ac5430edaf9ca8c74e028adaf0e48034f425
bee <minjihonim@naver.com>
2021-10-27 13:13:37 +0900

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA userSchema;
+GO
+CREATE SCHEMA buySchema;
+GO
+
+CREATE table userSchema.userTBL (id int); -- 스키마 생성
+GO
+CREAte table buySchema.buyTBL(num int);
+GO
+CREATE table buySchema.prodTBL(pid int); -- 스키마 생성
+GO
