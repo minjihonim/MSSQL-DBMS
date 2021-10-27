442213eb5e17d84193d84869176c470aa4af9ed4
bee <minjihonim@naver.com>
2021-10-27 09:48:26 +0900

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,18 @@
+CREATE TABLE userTBL (
+	userID CHAR(8) NOT NULL,
+	name nvarchar(10) NOT NULL,
+	birthYear int NOT NULL,
+	addr nchar(2) NOT NULL,
+	mobile1 char(3) null,
+	mobile2 char(8) null,
+	height smallint null,
+	mDate date null);
+GO
+Create table buyTBL (
+	num int NOT NULL,
+	userID char(8) NOT NULL,
+	prodNAME nchar(6) NOT NULL,
+	groupName nchar(4) null,
+	price int NOT NULL,
+	amount smallint NOT NULL );
+GO
